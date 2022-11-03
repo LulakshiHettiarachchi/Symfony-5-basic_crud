3d0da553390c0f7c7a291d9884b7aeb1bb4a2948
Lulakshi Hettiarachchi <lulakshinadunihettiarachchi@gmail.com>
2022-11-03 15:01:14 +0530

    synfony basic crud

---
+++ src/Entity/Project.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProjectRepository::class)]
+class Project
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Name = null;
+
+    #[ORM\Column(length: 3, nullable: true)]
+    private ?string $Age = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getAge(): ?string
+    {
+        return $this->Age;
+    }
+
+    public function setAge(?string $Age): self
+    {
+        $this->Age = $Age;
+
+        return $this;
+    }
+}
